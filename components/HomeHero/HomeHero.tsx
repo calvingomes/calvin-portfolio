b1483b79e2794a289dd2b6fc0bf5f0c41d7de3ec
calvingomes <calvingomes045@gmail.com>
2025-11-24 04:27:31 +0530

    add image for homehero

--- components/HomeHero/HomeHero.tsx
+++ components/HomeHero/HomeHero.tsx
@@ -6,21 +6,26 @@ export default function HomeHero() {
     <div className={styles.homeHero}>
       <p className={styles.heroDescriptionMobile}>I support Designers and Agencies with creative development</p>
       <h2 className={styles.heroTitle}>Frontend Developer</h2>
-      <Image
-        src="https://placehold.jp/340x290.png"
-        height={290}
-        width={340}
-        alt="Picture of the author"
-        className={styles.heroPicMobile}
-      />
+      
+      <div className={styles.heroPicMobileWrapper}>
+        <Image
+          src="/images/cal-portrait-1.jpg"
+          alt="Calvin portrait"
+          fill
+          className={styles.heroPicMobile}
+          sizes="360px"
+        />
+      </div>
 
-      <Image
-        src="https://placehold.jp/800x260.png"
-        height={260}
-        width={800}
-        alt="Picture of the author"
-        className={styles.heroPicDesktop}
-      />
+      <div className={styles.heroPicDesktopWrapper}>
+        <Image
+          src="/images/cal-portrait-1.jpg"
+          alt="Calvin portrait"
+          fill
+          className={styles.heroPicDesktop}
+          sizes="800px"
+        />
+      </div>
 
       <div className={styles.heroNameContainer}>
         <p className={styles.heroDescriptionDesktop}>I support Designers and Agencies with creative development</p>
