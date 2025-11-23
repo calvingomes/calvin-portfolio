c951e61a3578b020d33260546de2306256bebe40
calvingomes <calvingomes045@gmail.com>
2025-11-24 02:46:55 +0530

    update homehero comp / more ui changes

--- components/HomeHero/HomeHero.tsx
+++ components/HomeHero/HomeHero.tsx
@@ -4,25 +4,26 @@ import styles from './HomeHero.module.css';
 export default function HomeHero() {
   return (
     <div className={styles.homeHero}>
-      <p className={styles.heroDescription}>I support Designers and Agencies with creative development</p>
+      <p className={styles.heroDescriptionMobile}>I support Designers and Agencies with creative development</p>
       <h2 className={styles.heroTitle}>Frontend Developer</h2>
       <Image
-        src="https://placehold.jp/320x290.png"
+        src="https://placehold.jp/340x290.png"
         height={290}
-        width={320}
+        width={340}
         alt="Picture of the author"
         className={styles.heroPicMobile}
       />
 
       <Image
-        src="https://placehold.jp/720x260.png"
+        src="https://placehold.jp/800x260.png"
         height={260}
-        width={720}
+        width={800}
         alt="Picture of the author"
         className={styles.heroPicDesktop}
       />
 
       <div className={styles.heroNameContainer}>
+        <p className={styles.heroDescriptionDesktop}>I support Designers and Agencies with creative development</p>
         <h2 className={styles.heroName}>Calvin Gomes</h2>
       </div>
     </div>
